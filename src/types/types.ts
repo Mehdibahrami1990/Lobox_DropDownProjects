20f53874dc757ff832f578e8329e565387e95c8d
Mehdi Bahrami <mehdi.bahrami11990@gmail.com>
2025-05-12 12:14:50 +0330

    update: Create the required types for project

---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+export type SelectorOptionProps = {
+  label: string;
+  value: string;
+};
+export type SelectDropdownProps = {
+  options: SelectorOptionProps[];
+  onChange: (selected: SelectorOptionProps[]) => void;
+};
